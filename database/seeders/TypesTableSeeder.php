fc45f1aeeb3828cdee035f691cc8d5e5eb2e8217
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-07-10 17:35:49 +0200

    add function typestableseeder

---
+++ database/seeders/TypesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Generator as Faker;
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 100; $i++) {
+            $type = new Type();
+
+            $type->title = $faker->word();
+            $type->collabs = $faker->name();
+            $type->save();
+        }
+    }
+}
